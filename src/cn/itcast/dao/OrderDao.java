d59a7e89623be151a9bb3e7f5a8e1c617f50fa99
Herman_guo <674444326@qq.com>
2020-08-02 21:32:59 +0800

    'update eee'

--- src/cn/itcast/dao/OrderDao.java
+++ src/cn/itcast/dao/OrderDao.java
@@ -1,13 +1,1 @@
-package cn.itcast.dao;
-
-import java.sql.Connection;
-import java.util.List;
-
-import cn.itcast.vo.Order;
-import cn.itcast.vo.OrderItem;
-
-public interface OrderDao {
-
-aaaaaaaaaaa
-	
-}
+PPPPPPPPPPPPPPPPPPPPPPPPPPPP
